98c27b09f25a35cf28fa143b24fa3ee398191823
Kerem <krmtrk20@gmail.com>
2020-12-28 09:47:58 +0300

    Remove devtools from production env

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.modules.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import burgerBuilderReducer from './store/reducers/burgerBuilder'
-import orderReducer from './store/reducers/order'
-import authReducer from './store/reducers/auth'
-import thunk from 'redux-thunk';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.modules.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+import orderReducer from "./store/reducers/order";
+import authReducer from "./store/reducers/auth";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
-  burgerBuilder: burgerBuilderReducer,
-  order: orderReducer,
-  auth: authReducer,
-})
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer,
+    auth: authReducer,
+});
 
-const store = createStore(rootReducer, composeWithDevTools(
-  applyMiddleware(thunk)
-));
-
+const store =
+    process.env.NODE_ENV === "development"
+        ? createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+        : createStore(rootReducer, applyMiddleware(thunk));
 const app = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-)
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
 
-
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
