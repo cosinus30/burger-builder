7072ae47dfe0fa62b65b4b1aaf2d3cf62ccceaa7
Kerem <krmtrk20@gmail.com>
2020-12-28 09:29:03 +0300

    Refactor authentication text

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,7 @@ const NavigationItems = (props) => {
         <ul className={classes.NavigationItems}>
             <NavigationItem link="/" >Burger Builder</NavigationItem>
             {!props.isAuthenticated
-                ? <NavigationItem link="/auth">Authenticate</NavigationItem>
+                ? <NavigationItem link="/auth">Sign in</NavigationItem>
                 : (<React.Fragment>
                     <NavigationItem link="/orders">Orders</NavigationItem>
                     <NavigationItem NavigationItem link="/logout">Logout</NavigationItem>
