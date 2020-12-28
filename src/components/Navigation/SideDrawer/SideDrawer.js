ada10195e1551294ae6bf1c5faabc9810d9de0d9
Kerem <krmtrk20@gmail.com>
2020-12-28 09:28:50 +0300

    Add close sidedrawer when clicked

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import Logo from '../../Logo/Logo'
-import NavigationItems from '../NavigationItems/NavigationItems'
-import Backdrop from '../../UI/Backdrop/Backdrop'
-import classes from './SideDrawer.module.css'
-
-
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import classes from "./SideDrawer.module.css";
 
 const SideDrawer = (props) => {
-
     let attachedClasses = [classes.SideDrawer, classes.Close];
     if (props.open) {
-        attachedClasses = [classes.SideDrawer, classes.Open]
+        attachedClasses = [classes.SideDrawer, classes.Open];
     }
 
     return (
         <React.Fragment>
             <Backdrop show={props.open} clicked={props.closed} />
-            <div className={attachedClasses.join(' ')}>
+            <div className={attachedClasses.join(" ")} onClick={props.closed}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
-                <nav><NavigationItems isAuthenticated={props.isAuthenticated} /></nav>
-
+                <nav>
+                    <NavigationItems isAuthenticated={props.isAuthenticated} />
+                </nav>
             </div>
         </React.Fragment>
     );
-}
+};
 
-export default SideDrawer
+export default SideDrawer;
